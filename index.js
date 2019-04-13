8590d6f532c27e948915110a9861ea5c2f743355
liujun <liujun@dxwt.com>
2019-04-13 20:09:54 +0800

    1111

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-'use strict';
+/**
+ * CopyRight (C) 2017-2022 Alibaba Group Holding Limited.
+ * Created by Tw93 on 17/09/25
+ */
 
-import * as utils from './utils/util';
-export {utils}
+import ScreenshotDialog from './packages/screenshot-dialog';
+import Utils from './packages/utils';
+
+export {
+  ScreenshotDialog,
+  Utils
+};
